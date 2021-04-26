f8136c972ec12cc3523b84207831e83a8d50d993
Exordio <45700369+Exordio@users.noreply.github.com>
2021-04-26 03:09:30 +0300

    Копирование assets

--- clientCen.py
+++ clientCen.py
@@ -1,12 +1,12 @@
 import os
+import sys
 import json
+import shutil
 
 
-def readJson(path):
-    with open(path) as json_file:
-        data = json.load(json_file)
-
-    print(data)
+assetsIndexesPath = f'''{os.path.expanduser("~")}/AppData/Roaming/.minecraft/assets/indexes/'''
+assetsObjectsPath = f'''{os.path.expanduser("~")}/AppData/Roaming/.minecraft/assets/objects/'''
+genFolder = 'Client'
 
 
 def listAssetsDir(path):
@@ -16,15 +16,12 @@ def listAssetsDir(path):
     for i in range(len(listAssetsJsons)):
         assetsDict = {i: listAssetsJsons[i]}
         print(f'{i} - {listAssetsJsons[i]}')
-
     return assetsDict
 
 
 def selectAssets():
-    assetsPath = f'''{os.path.expanduser("~")}/AppData/Roaming/.minecraft/assets/indexes'''
     print('| Шаг 1. Выбор asset. Необходимо выбрать нужный ассет из меню ниже. |\n')
-    toSelectAssetsDict = listAssetsDir(assetsPath)
-
+    toSelectAssetsDict = listAssetsDir(assetsIndexesPath)
     correct = False
     while not correct:
         selectedAssetNum = input(": ")
@@ -34,8 +31,78 @@ def selectAssets():
         except KeyError:
             print('| Неверный номер меню, повторите ввод |')
 
+    # Создаём директорию для ассета
+    try:
+        os.mkdir(f'''{genFolder}/asset{toSelectAssetsDict[int(selectedAssetNum)].replace('.json', '')}''')
+    except FileExistsError:
+        print('| Папка для выбранного ассета уже существует, пропускаем создание. |')
+
+    return toSelectAssetsDict[int(selectedAssetNum)]
+
+
+def readJson(path):
+    with open(path) as json_file:
+        data = json.load(json_file)
+    return data
+
+
+def createAssetFsStructure(sa):
+    try:
+        os.mkdir(f'''{genFolder}/asset{sa.replace('.json', '')}/indexes''')
+    except FileExistsError:
+        print("| При создании папки индексов ассетов, было обнаружено что папка уже существует, пропускаем |")
+    try:
+        os.mkdir(f'''{genFolder}/asset{sa.replace('.json', '')}/objects''')
+    except FileExistsError:
+        print("| При создании папки объектов ассетов, было обнаружено что папка уже существует, пропускаем |")
+
+
+def createAssetObjectSubFolder(folder, sa):
+    try:
+        os.mkdir(f'''{genFolder}/asset{sa.replace('.json', '')}/objects/{folder}''')
+    except FileExistsError:
+        pass
+
+
+def copyAssetObj(assetHash, sa):
+    originalAssetObjPath = f'''{assetsObjectsPath}{assetHash[0:2]}/{assetHash}'''
+    targetAssetObjPath = f'''{genFolder}/asset{sa.replace('.json', '')}/objects/{assetHash[0:2]}/'''
+    sys.stdout.write(f'''\r| Копируем {assetHash} |''')
+    sys.stdout.flush()
+
+    shutil.copy(originalAssetObjPath, targetAssetObjPath)
+
+
+def copyAssetIndexesFile(sa):
+    print(f'\n| Копируем {sa} |')
+    originalAssetIndexPath = f'''{assetsIndexesPath}{sa}'''
+    targetAssetIndexPath = f'''{genFolder}/asset{sa.replace('.json', '')}/indexes/'''
+    shutil.copy(originalAssetIndexPath, targetAssetIndexPath)
+
+
+def findNCopyAsset(assetIndex, sa):
+    print('\n')
+    for i in assetIndex['objects']:
+        createAssetObjectSubFolder(assetIndex['objects'][i]['hash'][0:2], sa)
+        copyAssetObj(assetIndex['objects'][i]['hash'], sa)
+
+    copyAssetIndexesFile(sa)
+
+    print('\n| Копирование assets завершено. |')
+
 
 if __name__ == '__main__':
     print('|  GreatRay client generator 0.1 |')
-    selectAssets()
+    print('| Предварительно создаём новую папку для генерации клиента |')
+    try:
+        os.mkdir(genFolder)
+    except FileExistsError:
+        print('| Папка уже создана |')
+
+    selectedAsset = selectAssets()
+    assetsJsonObj = readJson(assetsIndexesPath + selectedAsset)
+
+    createAssetFsStructure(selectedAsset)
+
+    findNCopyAsset(assetsJsonObj, selectedAsset)
 
