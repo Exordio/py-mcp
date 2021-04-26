5412f317860659de383ac0391f75eb14f542d937
Exordio <45700369+Exordio@users.noreply.github.com>
2021-04-26 19:28:57 +0300

    hrenhren

--- goldenHand.py
+++ goldenHand.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 constants = {
     'api': {
@@ -15,10 +16,70 @@ constants = {
 }
 
 
-def request(url):
-    return requests.get(url).json()
+def request(url, json = False, content = False):
+    if json:
+        return requests.get(url).json()
+    if content:
+        return requests.get(url).content
+
+
+def download(url, dest, size, visibleName):
+    pass
+
+
+
+def getVersionManifest():
+    return request(constants['api']['versionsInfo'], json=True)
 
 if __name__ == '__main__':
-    print(request(constants['api']['versionsInfo']))
+    print('|  GreatRay client generator 0.1 |')
+    print('| Предварительно создаём новую папку для генерации клиента |')
+
+    versionsInfo = getVersionManifest()
+
+    versions = []
+    versionsNumbs = []
+
+    for i in versionsInfo['versions']:
+        if i['type'] == 'release':
+            versions.append(i)
+            versionsNumbs.append(i['id'])
+    print(versions)
+    print(versionsNumbs)
+
+    finded = False
+    while not finded:
+        versionSelect = input(' : ')
+        for i in versionsNumbs:
+            if i == versionSelect:
+                print('Версия найдена')
+                finded = True
+                break
+        if not finded:
+            print('Версия не найдена, повторите ввод.')
+
+    for i in versions:
+        if i['id'] == versionSelect:
+            versionData = i
+
+    try:
+        os.mkdir(constants['package']['outputPath'])
+    except FileExistsError:
+        print('| Папка уже создана |')
+
+    versionData = request(versionData['url'], json=True)
+    print(versionData['downloads']['client']['url'].split('/')[-1])
+
+    with open(f'''{constants['package']['outputPath']}/{versionData['downloads']['client']['url'].split('/')[-1]}''', 'wb') as vsd:
+        vsd.write(request(versionData['downloads']['client']['url'], content=True))
+
+
+
+
+
+
+
+
+
 
 
