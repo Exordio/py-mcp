4b82832bd1e328ca8317a8281af15a99588c9776
Exordio <45700369+Exordio@users.noreply.github.com>
2021-04-27 06:31:03 +0300

    возможность скачать снапшот

--- goldenHand.py
+++ goldenHand.py
@@ -15,7 +15,7 @@ constants = {
         'assetsDownloadBaseUrl': 'http://resources.download.minecraft.net',
     },
     'package': {
-        'outputPath': f'./output{datetime.now().time()}',
+        'outputPath': f'''./output {str(datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))}''',
         'tmpPath': './output/tmp',
         'librariesDir': 'libraries',
         'assetsDir': 'assets',
@@ -226,8 +226,22 @@ if __name__ == '__main__':
     versions = []
     versionsNumbs = []
 
+    print('| Выберете версионную ветку для загрузки (Указать можно цифрой) |')
+    print('1. Release\n2. Snapshot\n')
+    typeSelected = False
+    while not typeSelected:
+        versionType = input(' : ')
+        if versionType == '1':
+            versionType = 'release'
+            typeSelected = True
+        elif versionType == '2':
+            versionType = 'snapshot'
+            typeSelected = True
+        else:
+            print('| Введено неверное значение, повторите ввод |')
+
     for i in versionsInfo['versions']:
-        if i['type'] == 'release':
+        if i['type'] == versionType:
             versions.append(i)
             versionsNumbs.append(i['id'])
 
