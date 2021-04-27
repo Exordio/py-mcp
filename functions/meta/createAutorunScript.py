6f681703dc65b3c19e25684abba690c8fd6a889c
Exordio <45700369+Exordio@users.noreply.github.com>
2021-04-28 02:45:39 +0300

    some fixes

--- functions/meta/createAutorunScript.py
+++ functions/meta/createAutorunScript.py
@@ -9,7 +9,7 @@ import os
 separator = ';' if platform == 'windows' else ':'
 
 
-def createAutorunScript(versionIndex, assetIndex, versionType):
+def createAutorunScript(versionIndex, assetIndex, versionType, magicImpotantMushrooms):
     with ZipFile(f'''{constants['package']['outputPath']}/client.jar''', 'r') as zipObj:
         zipObj.extractall(f'''temp''')
 
@@ -38,8 +38,8 @@ for i in range(len(filenames)):
     runScript += f'''classPathFiles = '{separator}'.join(filenames)\n'''
     runScript += f'''arguments.append(f\'-cp client.jar{separator}''' + '''{classPathFiles}\')\n'''
     runScript += f'''arguments.append('{mainClassName}')
-arguments.append('--username Username')
-arguments.append('--gameDir "' + os.path.abspath('.') + '"')
+arguments.append('{'--username username' if not magicImpotantMushrooms else 'username null'}')\n'''
+    runScript += '''arguments.append('--gameDir "' + os.path.abspath('.') + '"')
 arguments.append('--workDir "' + os.path.abspath('.') + '"')
 arguments.append('--assetsDir "' + os.path.abspath('assets') + '"')
 arguments.append('--assetIndex {assetIndex}')
--- goldenHand.py
+++ goldenHand.py
@@ -1,6 +1,6 @@
 import time
 
-from config.config import constants, autoRun
+from config.config import constants, autoDelete, autoRun
 from functions.fs.createClientFolders import createClientFolders
 from functions.getters.getAssets import getAssets
 from functions.getters.getVersionData import getVersionData
@@ -13,36 +13,42 @@ from functions.selectors.selectVersion import selectVersion
 from functions.selectors.selectVersionType import selectVersionType
 from functions.meta.createAutorunScript import createAutorunScript, run
 from datetime import datetime
+import shutil
+import sys
 import os
 
 if __name__ == '__main__':
     print('|  Exord minecraft client generator 0.1  |\n')
 
-    if os.path.exists(constants['package']['outputPath']):
-        print("Директория с клиентом уже существует. Клиент будет запущен без скачивания (если включено в конфиге). Даём 3 секунды на подумать...")
+    if os.path.exists(constants['package']['outputPath']) and autoDelete:
+        shutil.rmtree('output')
+    elif os.path.exists(constants['package']['outputPath']) and not autoDelete:
+        print(
+            "Директория с клиентом уже существует. Клиент будет запущен без скачивания (если включено в конфиге). Даём 3 секунды на подумать...")
         time.sleep(3)
         if autoRun:
             run()
-    else:
-        # Получаем манифест всех версий minecraft
-        versionsInfo = getVersionManifest()
-        # Выбираем тип выпуска minecraft
-        versions, versionsNumbs, versionType = selectVersionType(versionsInfo)
-        # Выбираем номер версии, и получаем versionData
-        versionData = getVersionData(selectVersion(versions, versionsNumbs))
-        # Создание всех нужных директорий и поддиректорий
-        createClientFolders(versionData)
-        # # Загрузка клиента
-        getClient(versionData)
-        # Загрузка сервера
-        getServer(versionData)
-        # Загрузка библиотек
-        getLibraries(versionData)
-        # Загрузка нативов
-        getNatives(versionData)
-        # Загрузка ассетов
-        getAssets(versionData)
-        # Создание скрипта запуска
-        createAutorunScript(versionData['id'], versionData['assetIndex']['id'], versionType)
+            sys.exit(0)
 
-        print(f'\n| {datetime.now().time()} Сборка клиента завершена! |')
+    # Получаем манифест всех версий minecraft
+    versionsInfo = getVersionManifest()
+    # Выбираем тип выпуска minecraft
+    versions, versionsNumbs, versionType = selectVersionType(versionsInfo)
+    # Выбираем номер версии, и получаем versionData
+    versionData = getVersionData(selectVersion(versions, versionsNumbs))
+    # Создание всех нужных директорий и поддиректорий
+    createClientFolders(versionData)
+    # # Загрузка клиента
+    getClient(versionData)
+    # Загрузка сервера
+    getServer(versionData)
+    # Загрузка библиотек
+    getLibraries(versionData)
+    # Загрузка нативов
+    getNatives(versionData)
+    # Загрузка ассетов
+    magicImpotantMushrooms = getAssets(versionData)
+    # Создание скрипта запуска
+    createAutorunScript(versionData['id'], versionData['assetIndex']['id'], versionType, magicImpotantMushrooms)
+
+    print(f'\n| {datetime.now().time()} Сборка клиента завершена! |')
