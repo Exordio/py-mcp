058710c90ebfa9c44dd3cc1e40e63a68c33fe108
Administrator <admin@example.com>
2021-04-28 02:23:49 +0400

    refactor

--- functions/meta/createAutorunScript.py
+++ functions/meta/createAutorunScript.py
@@ -1,6 +1,7 @@
 from config.config import platform, autoRun
 from datetime import datetime
 from zipfile import ZipFile
+from config.config import constants
 
 import shutil
 import os
@@ -9,34 +10,34 @@ separator = ';' if platform == 'windows' else ':'
 
 
 def createAutorunScript(versionIndex, assetIndex, versionType):
+    with ZipFile(f'''{constants['package']['outputPath']}/client.jar''', 'r') as zipObj:
+        zipObj.extractall(f'''temp''')
 
-    # with ZipFile(f'''output/client.jar''', 'r') as zipObj:
-    #     zipObj.extractall(f'''temp''')
-    #
-    # with open('temp/META-INF/MANIFEST.MF', 'r') as mainClassManifest:
-    #     for i in mainClassManifest.readlines():
-    #         if i.startswith('Main-Class:'):
-    #             mainClassName = i.strip().split(' ')[-1]
-    # shutil.rmtree('temp')
+    with open('temp/META-INF/MANIFEST.MF', 'r') as mainClassManifest:
+        for i in mainClassManifest.readlines():
+            if i.startswith('Main-Class:'):
+                mainClassName = i.strip().split(' ')[-1]
+    if mainClassName == 'net.minecraft.client.Main':
+        mainClassName = 'net.minecraft.client.main.Main'
+    shutil.rmtree('temp')
 
     print(f'|\n {datetime.now().time()} Создаем скрипт запуска |')
-    autoRun = '''import os
+    runScript = '''import os
 arguments = []
 arguments.append('java')
-arguments.append('-Djava.library.path=natives')
+arguments.append('-Djava.library.path="' + os.path.abspath('natives') + '"')
 arguments.append('-XX:HeapDumpPath=ThisTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump')
 classPath = []
 _, _, filenames = next(os.walk('libraries'))
 for i in range(len(filenames)):
     filenames[i] = f'libraries/{filenames[i]}'
-    '''
-    autoRun += f'''classPathFiles = '{separator}'.join(filenames)\n'''
-    autoRun += f'''arguments.append(f\'-cp client.jar{separator}''' + '''{classPathFiles}\')\n'''
-    # autoRun += f'''arguments.append(f'{mainClassName}')
-    autoRun += f'''arguments.append('net.minecraft.client.main.Main')
+'''
+    runScript += f'''classPathFiles = '{separator}'.join(filenames)\n'''
+    runScript += f'''arguments.append(f\'-cp client.jar{separator}''' + '''{classPathFiles}\')\n'''
+    runScript += f'''arguments.append('{mainClassName}')
 arguments.append('--username Username')
 arguments.append('--gameDir "' + os.path.abspath('.') + '"')
-arguments.append('--assetsDir "' + os.path.abspath('assets{versionIndex}') + '"')
+arguments.append('--assetsDir "' + os.path.abspath('assets') + '"')
 arguments.append('--assetIndex {assetIndex}')
 arguments.append('--uuid 0')
 arguments.append('--accessToken 0')
@@ -44,7 +45,7 @@ arguments.append('--userType --mojang')
 arguments.append('--versionType --{versionType}')
 arguments.append('--version {versionIndex}')
 '''
-    autoRun += '''arguments.append('--userProperties {}')
+    runScript += '''arguments.append('--userProperties {}')
 launchStr = f\'\'\' \'\'\'.join(arguments)
 print(launchStr)
 launch = os.system(launchStr)
@@ -53,13 +54,17 @@ print(f'{launch}')
 '''
     print(f'| {datetime.now().time()} Скрипт запуска создан! |')
 
-    with open('output/start.py', 'w') as startScript:
-        startScript.write(autoRun)
+    with open(f"{constants['package']['outputPath']}/start.py", 'w') as startScript:
+        startScript.write(runScript)
 
     if autoRun:
-        cur_dir = os.path.abspath(".")
-        os.chdir(f'{cur_dir}/output')
-        if platform == 'windows':
-            os.system(f'\"{cur_dir}/venv/scripts/python\" start.py')
-        else:
-            os.system(f'\"{cur_dir}/venv/bin/python\" start.py')
+        run()
+
+
+def run():
+    cur_dir = os.path.abspath(".")
+    os.chdir(f"{cur_dir}/{constants['package']['outputPath']}")
+    if platform == 'windows':
+        os.system(f'\"{cur_dir}/venv/scripts/python\" start.py')
+    else:
+        os.system(f'\"{cur_dir}/venv/bin/python\" start.py')
